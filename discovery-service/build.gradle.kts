8f323360e00ce768a66f15990610910215e3c5f1
Timofey Kozlov <iZouiR@jotaro.ru>
2024-06-27 00:33:35 +0300

    add discovery microservice

---
+++ discovery-service/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.0"
+    id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "org.izouir"
+version = "1.0-INTERNSHIP"
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2023.0.2"
+
+dependencies {
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+    implementation("org.springframework.cloud:spring-cloud-starter-config:4.1.2")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
---
+++ discovery-service/src/main/java/org/izouir/discovery_service/DiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package org.izouir.discovery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+}
