13084e11fe7bcfa1617b8daa2cd77812f9ca5e6d
Timofey Kozlov <iZouiR@jotaro.ru>
2024-06-27 00:27:04 +0300

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "Store Manager"
+
+includeBuild("config-service")
+includeBuild("discovery-service")
+includeBuild("gateway-service")
+includeBuild("inventory-service")
+includeBuild("order-service")
+includeBuild("product-service")
