bcd9f7ecbf15207b43d68f9eb64b64d6efc0bdd1
Timofey Kozlov <iZouiR@jotaro.ru>
2024-07-04 01:04:01 +0300

    refactor: make StoredProductMapper an utility class with lombok

--- order-service/src/main/java/org/izouir/order_service/mapper/StoredProductMapper.java
+++ order-service/src/main/java/org/izouir/order_service/mapper/StoredProductMapper.java
@@ -1,15 +1,17 @@
 package org.izouir.order_service.mapper;
 
+import lombok.experimental.UtilityClass;
 import org.izouir.order_service.dto.StoredProductDto;
 import org.izouir.order_service.entity.StoredProduct;
 
+@UtilityClass
 public class StoredProductMapper {
-    public static StoredProductDto toDto(final StoredProduct storedProduct) {
+    public StoredProductDto toDto(final StoredProduct storedProduct) {
         return StoredProductDto.builder()
                 .id(storedProduct.getId())
                 .product(ProductMapper.toDto(storedProduct.getProduct()))
                 .storeId(storedProduct.getStore().getId())
-                .stored(storedProduct.getStored())
+                .quantity(storedProduct.getQuantity())
                 .build();
     }
 }
