9f72a9c01dc997ced40cc0b073cf10e99881cf24
Timofey Kozlov <iZouiR@jotaro.ru>
2024-07-03 22:45:31 +0300

    refactor: make ProductMapper an utility class with lombok

--- inventory-service/src/main/java/org/izouir/inventory_service/mapper/ProductMapper.java
+++ inventory-service/src/main/java/org/izouir/inventory_service/mapper/ProductMapper.java
@@ -1,10 +1,12 @@
 package org.izouir.inventory_service.mapper;
 
+import lombok.experimental.UtilityClass;
 import org.izouir.inventory_service.dto.ProductDto;
 import org.izouir.inventory_service.entity.Product;
 
+@UtilityClass
 public class ProductMapper {
-    public static ProductDto toDto(final Product product) {
+    public ProductDto toDto(final Product product) {
         return ProductDto.builder()
                 .id(product.getId())
                 .label(product.getLabel())
