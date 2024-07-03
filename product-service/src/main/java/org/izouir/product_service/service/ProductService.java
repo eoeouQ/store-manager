13c1d65e923702f48ca1d87cc3c099064ef0830a
Timofey Kozlov <iZouiR@jotaro.ru>
2024-07-03 22:01:37 +0300

    refactor: split chain-methods to separate lines, rename vars, move exception strings to const, make update method not to store null in db

--- product-service/src/main/java/org/izouir/product_service/service/ProductService.java
+++ product-service/src/main/java/org/izouir/product_service/service/ProductService.java
@@ -13,35 +13,43 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class ProductService {
-    private final ProductRepository repository;
+    private final ProductRepository productRepository;
+
+    private static final String PRODUCT_NOT_FOUND_MESSAGE = "Product with id %s not found";
 
     @Transactional
     public List<ProductDto> findAll() {
-        return repository.findAll().stream().map(ProductMapper::toDto).toList();
+        return productRepository.findAll().stream()
+                .map(ProductMapper::toDto)
+                .toList();
     }
 
     @Transactional
     public ProductDto find(final Long id) {
-        return ProductMapper.toDto(repository.findById(id).orElseThrow(
-                () -> new ProductNotFoundException("Product with id " + id + " not found")));
+        return ProductMapper.toDto(productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException(String.format(PRODUCT_NOT_FOUND_MESSAGE, id))));
     }
 
     @Transactional
     public ProductDto save(final ProductDto productDto) {
-        return ProductMapper.toDto(repository.save(ProductMapper.toEntity(productDto)));
+        return ProductMapper.toDto(productRepository.save(ProductMapper.toEntity(productDto)));
     }
 
     @Transactional
     public ProductDto update(final ProductDto productDto) {
-        final var product = repository.findById(productDto.getId()).orElseThrow(
-                () -> new ProductNotFoundException("Product with id " + productDto.getId() + " not found"));
-        product.setLabel(productDto.getLabel());
-        product.setPrice(productDto.getPrice());
-        return ProductMapper.toDto(repository.save(product));
+        final var product = productRepository.findById(productDto.getId()).orElseThrow(
+                () -> new ProductNotFoundException(String.format(PRODUCT_NOT_FOUND_MESSAGE, productDto.getId())));
+        if (productDto.getLabel() != null) {
+            product.setLabel(productDto.getLabel());
+        }
+        if (productDto.getPrice() != null) {
+            product.setPrice(productDto.getPrice());
+        }
+        return ProductMapper.toDto(productRepository.save(product));
     }
 
     @Transactional
     public void delete(final Long id) {
-        repository.deleteById(id);
+        productRepository.deleteById(id);
     }
 }
