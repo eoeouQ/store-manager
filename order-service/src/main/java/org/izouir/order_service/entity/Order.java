12c62b95414b2c2015037f78f8e157bd91f6bba4
Timofey Kozlov <iZouiR@jotaro.ru>
2024-07-04 01:00:24 +0300

    change definition of Order entity

--- order-service/src/main/java/org/izouir/order_service/entity/Order.java
+++ order-service/src/main/java/org/izouir/order_service/entity/Order.java
@@ -7,30 +7,34 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "orders")
+@Table(name = "order")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", columnDefinition = "BIGSERIAL PRIMARY KEY")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "stored_product_id", columnDefinition = "BIGINT REFERENCES stored_products (id)")
-    private StoredProduct storedProduct;
+    @Column(name = "user_id", columnDefinition = "BIGINT NOT NULL")
+    private Long userId;
+
+    @Column(name = "total_price", columnDefinition = "INT NOT NULL")
+    private Integer totalPrice;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "status", columnDefinition = "VARCHAR(32) NOT NULL")
     private OrderStatus status;
 
-    @Column(name = "quantity", columnDefinition = "INTEGER CHECK ( quantity > 0)")
-    private Integer quantity;
+    @Column(name = "date", columnDefinition = "TIMESTAMP NOT NULL DEFAULT NOW()")
+    private Timestamp date;
 
-    @Column(name = "placed_at", columnDefinition = "TIMESTAMP NOT NULL DEFAULT NOW()")
-    private Timestamp placedAt;
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", columnDefinition = "BIGINT REFERENCES order (id)")
+    private List<OrderPosition> positions;
 }
