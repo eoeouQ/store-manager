2e20721ac43df41e0f9891a7a54428add75e65fa
Timofey Kozlov <iZouiR@jotaro.ru>
2024-07-23 20:44:18 +0300

    make ids in db use sequences

--- db-migration-service/src/main/resources/migration/V1__init_db.sql
+++ db-migration-service/src/main/resources/migration/V1__init_db.sql
@@ -1,20 +1,23 @@
+CREATE SEQUENCE IF NOT EXISTS store_sequence AS BIGINT;
 CREATE TABLE IF NOT EXISTS store
 (
-    id       BIGSERIAL PRIMARY KEY,
+    id       BIGINT PRIMARY KEY DEFAULT nextval('store_sequence'),
     name     VARCHAR(64) NOT NULL,
     location VARCHAR(64) NOT NULL
 );
 
+CREATE SEQUENCE IF NOT EXISTS product_sequence AS BIGINT;
 CREATE TABLE IF NOT EXISTS product
 (
-    id    BIGSERIAL PRIMARY KEY,
+    id    BIGINT PRIMARY KEY DEFAULT nextval('product_sequence'),
     label VARCHAR(64) UNIQUE NOT NULL,
     price INT                NOT NULL CHECK ( price > 0 )
 );
 
+CREATE SEQUENCE IF NOT EXISTS stored_product_sequence AS BIGINT;
 CREATE TABLE IF NOT EXISTS stored_product
 (
-    id         BIGSERIAL PRIMARY KEY,
+    id         BIGINT PRIMARY KEY DEFAULT nextval('stored_product_sequence'),
     product_id BIGINT REFERENCES product (id),
     store_id   BIGINT REFERENCES store (id),
     quantity   INT CHECK ( quantity >= 0 )
@@ -23,9 +26,10 @@ CREATE TABLE IF NOT EXISTS stored_product
 CREATE TYPE ORDER_STATUS
 AS ENUM ('STATUS_CREATED', 'STATUS_DELIVERING', 'STATUS_DELIVERED');
 
-CREATE TABLE IF NOT EXISTS order
+CREATE SEQUENCE IF NOT EXISTS order_sequence AS BIGINT;
+CREATE TABLE IF NOT EXISTS "order"
 (
-    id          BIGSERIAL PRIMARY KEY,
+    id          BIGINT PRIMARY KEY    DEFAULT nextval('order_sequence'),
     user_id     BIGINT       NOT NULL,
     total_price INT          NOT NULL,
     status      ORDER_STATUS NOT NULL,
@@ -35,7 +39,7 @@ CREATE TABLE IF NOT EXISTS order
 CREATE TABLE IF NOT EXISTS order_position
 (
     stored_product_id BIGINT REFERENCES stored_product (id),
-    order_id          BIGINT REFERENCES order (id),
+    order_id          BIGINT REFERENCES "order" (id),
     quantity          INT CHECK ( quantity > 0),
     PRIMARY KEY (stored_product_id, order_id)
 );
