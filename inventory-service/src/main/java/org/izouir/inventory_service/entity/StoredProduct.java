91c100039f68e81f75443424a06dbdf6130d610b
Timofey Kozlov <iZouiR@jotaro.ru>
2024-07-26 11:13:48 +0300

    refactor: make id in StoredProduct embedded

--- inventory-service/src/main/java/org/izouir/inventory_service/entity/StoredProduct.java
+++ inventory-service/src/main/java/org/izouir/inventory_service/entity/StoredProduct.java
@@ -1,6 +1,12 @@
 package org.izouir.inventory_service.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,10 +19,8 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "stored_product")
 public class StoredProduct {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", columnDefinition = "BIGSERIAL PRIMARY KEY")
-    private Long id;
+    @EmbeddedId
+    StoredProductKey id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id", columnDefinition = "BIGINT REFERENCES product (id)")
