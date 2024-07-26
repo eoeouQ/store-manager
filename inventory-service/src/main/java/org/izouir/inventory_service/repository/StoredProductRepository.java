8a505c64a7d3ed37260a11a398626fffc40e6541
Timofey Kozlov <iZouiR@jotaro.ru>
2024-07-26 11:58:42 +0300

    make StoredProductRepository use StoredProductKey as id type instead of Long

--- inventory-service/src/main/java/org/izouir/inventory_service/repository/StoredProductRepository.java
+++ inventory-service/src/main/java/org/izouir/inventory_service/repository/StoredProductRepository.java
@@ -1,9 +1,10 @@
 package org.izouir.inventory_service.repository;
 
 import org.izouir.inventory_service.entity.StoredProduct;
+import org.izouir.inventory_service.entity.StoredProductKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StoredProductRepository extends JpaRepository<StoredProduct, Long> {
+public interface StoredProductRepository extends JpaRepository<StoredProduct, StoredProductKey> {
 }
