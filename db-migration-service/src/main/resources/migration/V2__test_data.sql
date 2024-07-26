650081a6fb2d5974faf9c75c3522a7fe9f8faad1
Timofey Kozlov <iZouiR@jotaro.ru>
2024-07-26 11:28:12 +0300

    update db testing data

--- db-migration-service/src/main/resources/migration/V2__test_data.sql
+++ db-migration-service/src/main/resources/migration/V2__test_data.sql
@@ -21,24 +21,24 @@ VALUES (1, 3, 17);
 INSERT INTO stored_product (product_id, store_id, quantity)
 VALUES (3, 2, 121);
 
-INSERT INTO order_position (stored_product_id, order_id, quantity)
-VALUES (1, 1, 10);
-INSERT INTO order_position (stored_product_id, order_id, quantity)
-VALUES (2, 1, 15);
-INSERT INTO order_position (stored_product_id, order_id, quantity)
-VALUES (1, 2, 20);
-INSERT INTO order_position (stored_product_id, order_id, quantity)
-VALUES (3, 2, 10);
-INSERT INTO order_position (stored_product_id, order_id, quantity)
-VALUES (4, 2, 15);
-INSERT INTO order_position (stored_product_id, order_id, quantity)
-VALUES (2, 3, 25);
-INSERT INTO order_position (stored_product_id, order_id, quantity)
-VALUES (4, 3, 50);
+INSERT INTO order_position (order_id, product_id, store_id, quantity)
+VALUES (1, 1, 1, 10);
+INSERT INTO order_position (order_id, product_id, store_id, quantity)
+VALUES (1, 2, 1, 15);
+INSERT INTO order_position (order_id, product_id, store_id, quantity)
+VALUES (2, 1, 1, 20);
+INSERT INTO order_position (order_id, product_id, store_id, quantity)
+VALUES (2, 1, 3, 10);
+INSERT INTO order_position (order_id, product_id, store_id, quantity)
+VALUES (2, 3, 2, 15);
+INSERT INTO order_position (order_id, product_id, store_id, quantity)
+VALUES (3, 2, 1, 25);
+INSERT INTO order_position (order_id, product_id, store_id, quantity)
+VALUES (3, 3, 2, 50);
 
-INSERT INTO order (user_id, total_price, status, date)
+INSERT INTO "order" (user_id, total_price, status, date)
 VALUES (1, 120, 'STATUS_CREATED', NOW());
-INSERT INTO order (user_id, total_price, status, dat)
+INSERT INTO "order" (user_id, total_price, status, date)
 VALUES (2, 270, 'STATUS_DELIVERED', NOW());
-INSERT INTO order (user_id, total_price, status, dat)
+INSERT INTO "order" (user_id, total_price, status, date)
 VALUES (3, 1090, 'STATUS_CREATED', NOW());
