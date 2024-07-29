1cf727c4f53fa062fa0c3754e4c7fbcd5ebbdee3
Timofey Kozlov <iZouiR@jotaro.ru>
2024-07-29 21:54:03 +0300

    add repositories for product and store

---
+++ order-service/src/main/java/org/izouir/order_service/repository/StoreRepository.java
@@ -0,0 +1,9 @@
+package org.izouir.order_service.repository;
+
+import org.izouir.order_service.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Long> {
+}
