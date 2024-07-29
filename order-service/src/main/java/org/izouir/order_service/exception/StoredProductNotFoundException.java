d1ac8662f5a707793a9e8d702f00e7636ba55262
Timofey Kozlov <iZouiR@jotaro.ru>
2024-07-29 21:46:58 +0300

    refactor: remove unused classes

--- order-service/src/main/java/org/izouir/order_service/exception/StoredProductNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.izouir.order_service.exception;
-
-import jakarta.persistence.EntityNotFoundException;
-
-public class StoredProductNotFoundException extends EntityNotFoundException {
-    public StoredProductNotFoundException(final String message) {
-        super(message);
-    }
-}
--- order-service/src/main/java/org/izouir/order_service/repository/StoredProductRepository.java
+++
@@ -1,9 +0,0 @@
-package org.izouir.order_service.repository;
-
-import org.izouir.order_service.entity.StoredProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StoredProductRepository extends JpaRepository<StoredProduct, Long> {
-}
