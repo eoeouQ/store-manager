584f527c7c4885c7f40bbcbbf5b62c36eeb53aeb
Timofey Kozlov <iZouiR@jotaro.ru>
2024-07-31 19:39:22 +0300

    update place method test of OrderServiceImplTest

--- order-service/src/test/java/org/izouir/order_service/service/impl/OrderServiceImplTest.java
+++ order-service/src/test/java/org/izouir/order_service/service/impl/OrderServiceImplTest.java
@@ -1,8 +1,5 @@
 package org.izouir.order_service.service.impl;
 
-import okhttp3.mockwebserver.MockResponse;
-import okhttp3.mockwebserver.MockWebServer;
-import org.apache.http.HttpHeaders;
 import org.izouir.order_service.dto.PlaceOrderRequestDto;
 import org.izouir.order_service.entity.Order;
 import org.izouir.order_service.entity.OrderPosition;
@@ -13,26 +10,17 @@ import org.izouir.order_service.entity.Store;
 import org.izouir.order_service.entity.StoreLocation;
 import org.izouir.order_service.exception.OrderNotFoundException;
 import org.izouir.order_service.mapper.OrderPositionMapper;
-import org.izouir.order_service.repository.OrderPositionRepository;
 import org.izouir.order_service.repository.OrderRepository;
-import org.izouir.order_service.repository.ProductRepository;
-import org.izouir.order_service.repository.StoreRepository;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import java.io.IOException;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.ArrayList;
@@ -55,31 +43,9 @@ public class OrderServiceImplTest {
     @Mock
     private OrderRepository orderRepository;
     @Mock
-    private ProductRepository productRepository;
-    @Mock
-    private StoreRepository storeRepository;
-    @Mock
-    private OrderPositionRepository orderPositionRepository;
+    private OrderPositionServiceImpl orderPositionService;
 
     private Order order;
-    private Product product;
-    private Store store;
-
-    @Spy
-    private static WebClient inventoryWebClient;
-    private static MockWebServer mockWebServer;
-
-    @BeforeAll
-    public static void setUp() throws IOException {
-        mockWebServer = new MockWebServer();
-        mockWebServer.start();
-        inventoryWebClient = WebClient.create(mockWebServer.url("/").toString());
-    }
-
-    @AfterAll
-    public static void tearDown() throws IOException {
-        mockWebServer.close();
-    }
 
     @BeforeEach
     public void init() {
@@ -90,12 +56,12 @@ public class OrderServiceImplTest {
                 .status(OrderStatus.STATUS_CREATED)
                 .date(Timestamp.from(Instant.now()))
                 .build();
-        product = Product.builder()
+        final var product = Product.builder()
                 .id(1L)
                 .label("TEST")
                 .price(1)
                 .build();
-        store = Store.builder()
+        final var store = Store.builder()
                 .id(1L)
                 .name("TEST")
                 .location(StoreLocation.LOCATION_BELARUS)
@@ -122,17 +88,6 @@ public class OrderServiceImplTest {
                 .build();
         when(orderRepository.save(Mockito.any(Order.class)))
                 .thenReturn(order);
-        when(productRepository.findById(Mockito.any(Long.class)))
-                .thenReturn(Optional.of(product));
-        when(storeRepository.findById(Mockito.any(Long.class)))
-                .thenReturn(Optional.of(store));
-        when(orderPositionRepository.save(Mockito.any(OrderPosition.class)))
-                .thenReturn(order.getPositions().get(0));
-        mockWebServer.enqueue(
-                new MockResponse().setResponseCode(200)
-                        .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                        .setBody("")
-        );
 
         final var orderDto = orderService.place(placeOrderRequestDto);
 
@@ -148,8 +103,7 @@ public class OrderServiceImplTest {
         assertEquals(orderPositionDto.getQuantity(), orderPosition.getQuantity());
 
         verify(orderRepository, times(1)).save(Mockito.any(Order.class));
-        verify(orderPositionRepository, times(1)).save(Mockito.any(OrderPosition.class));
-        verify(inventoryWebClient, times(1)).post();
+        verify(orderPositionService, times(1)).place(Mockito.any(Order.class), Mockito.any());
     }
 
     @Test
