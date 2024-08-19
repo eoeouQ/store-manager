0dce63db27073e99234d46329a3585471a2dfc37
Timofey Kozlov <iZouiR@jotaro.ru>
2024-08-19 13:43:58 +0300

    move ControllerExceptionHandlers to exception packages

--- product-service/src/main/java/org/izouir/product_service/aspect/LoggingAspect.java
+++ product-service/src/main/java/org/izouir/product_service/aspect/LoggingAspect.java
@@ -3,7 +3,6 @@ package org.izouir.product_service.aspect;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.izouir.product_service.controller.advice.ControllerExceptionHandlingAdvice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -11,9 +10,9 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LoggingAspect {
-    private static final Logger logger = LoggerFactory.getLogger(ControllerExceptionHandlingAdvice.class);
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Before("execution(* org.izouir.product_service.controller.advice.*.*(..))")
+    @Before("execution(* org.izouir.product_service.exception.*.*(..))")
     public void beforeAllExceptionHandlingAdvices(final JoinPoint joinPoint) {
         final var exception = (RuntimeException) joinPoint.getArgs()[0];
         logger.error(exception.getMessage());
