1543842f6d29f27530581c25e5ed1c25f35215f4
Timofey Kozlov <iZouiR@jotaro.ru>
2024-08-28 12:40:48 +0300

    rename order service's filter endpoint to search

--- order-service/src/main/java/org/izouir/order_service/controller/api/OrderAPI.java
+++ order-service/src/main/java/org/izouir/order_service/controller/api/OrderAPI.java
@@ -6,7 +6,13 @@ import org.izouir.order_service.dto.FiltersRequestDto;
 import org.izouir.order_service.dto.OrderDto;
 import org.izouir.order_service.dto.PlaceOrderRequestDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -30,6 +36,6 @@ public interface OrderAPI {
     ResponseEntity<List<OrderDto>> getOrderHistory();
 
     @Operation(summary = "Filtered orders")
-    @PostMapping("/filter")
+    @PostMapping("/search")
     ResponseEntity<List<OrderDto>> getOrdersFiltered(@RequestBody final FiltersRequestDto request);
 }
